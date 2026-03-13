0b0d5dea8ade6e2a7d331f13a098d1ea957aac3b
Snehaa <snehavsharma9125@gmail.com>
2026-03-13 22:31:32 +0530

    learning loops

---
+++ Day 5/loops.py
@@ -0,0 +1,6 @@
+fruits=["apple","mango","pear"]
+for fruit in fruits:   #for loop
+     print(fruit)       #prints the elements in the list
+     print(fruit +" pie")  
+
+
---
+++ Day 5/range.py
@@ -0,0 +1,7 @@
+# for num in range(1,10):  #does not include 10
+#     print(num)
+
+for n in range(1,11,3):  #prints 1 to 10 skipping multiples of 3
+    print(n)  
+
+
