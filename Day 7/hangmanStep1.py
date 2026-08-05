1df62a500075bed13cc0a53e505d998379044e7a
SNEHAVSHARMA <167785416+SNEHAVSHARMA@users.noreply.github.com>
2026-08-05 17:42:17 +0530

    Update hangmanStep1.py

--- Day 7/hangmanStep1.py
+++ Day 7/hangmanStep1.py
@@ -1,1 +1,12 @@
+import random
+word_list=["apple","girl","beach","country"]
+chosen_word=random.choice(word_list)                 #chooses a random word from the list
+print(chosen_word)
+guess_letter=input("Guess a letter:").lower()        #makes lower case
+print(guess_letter)
 
+for letter in chosen_word:                           
+    if(letter==guess_letter):
+        print("Right")
+    else:
+        print("Wrong")
